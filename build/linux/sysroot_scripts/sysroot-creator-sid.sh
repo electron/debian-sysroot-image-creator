e6615c06ef52dc025a86dea968734528de6a50b0
deepak1556 <hop2deep@gmail.com>
2020-09-11 22:51:18 -0700

    chore: [XProto] Remove xcbproto from sysroots
    https://chromium-review.googlesource.com/c/chromium/src/+/2276730

--- build/linux/sysroot_scripts/sysroot-creator-sid.sh
+++ build/linux/sysroot_scripts/sysroot-creator-sid.sh
@@ -359,7 +359,6 @@ DEBIAN_PACKAGES="\
   libzstd1
   linux-libc-dev
   mesa-common-dev
-  python-xcbgen
   shared-mime-info
   speech-dispatcher
   uuid-dev
@@ -376,7 +375,6 @@ DEBIAN_PACKAGES="\
   x11proto-scrnsaver-dev
   x11proto-xext-dev
   x11proto-xinerama-dev
-  xcb-proto
   zlib1g
   zlib1g-dev
 "
