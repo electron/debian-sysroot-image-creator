e4e8fc0c47d40a08674d0ff3eee871c23ddfa9ee
Cheng Zhao <zcbenz@gmail.com>
2023-07-27 15:59:09 +0900

    feat: add "URL" field to sysroots.json

--- build/linux/sysroot_scripts/build_and_upload.py
+++ build/linux/sysroot_scripts/build_and_upload.py
@@ -16,6 +16,7 @@ import subprocess
 import sys
 
 ARCHES = ("amd64", "i386", "armhf", "arm64", "armel", "mipsel", "mips64el")
+DEFAULT_URL_PREFIX = "https://dev-cdn.electronjs.org/linux-sysroots"
 
 
 def sha1sumfile(filename):
@@ -52,6 +53,7 @@ def build_and_upload(script_path, distro, release, key, arch, lock):
       "Sha1Sum": sha1sum,
       "SysrootDir": sysroot_dir,
       "Tarball": tarball,
+      "URL": DEFAULT_URL_PREFIX,
   }
   with lock:
     fname = os.path.join(script_dir, "sysroots.json")
